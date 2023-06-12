f23e7dc64fae3bde347d466c56805e2135125d8d
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-12 22:09:37 +0300

    complete task-05

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+function changeOutput(event) {
+  nameOutput.textContent = event.currentTarget.value.trim();
+  if (!event.currentTarget.value) {
+    nameOutput.textContent = "Anonymous";
+  }
+}
+
+nameInput.addEventListener("input", changeOutput);
