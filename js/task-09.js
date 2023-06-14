92d2fe83ee753b348e565f6530e3998736ef68d2
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-14 19:45:51 +0300

    task-09 completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+function setColor() {
+  colorValue.textContent = getRandomHexColor();
+  body.style.backgroundColor = colorValue.textContent;
+}
+
+button.addEventListener("click", setColor);
