52d4b2089fa0cd06ba25cc80445703b34e8161b8
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-14 16:01:20 +0300

    complete task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const loginForm = document.querySelector(".login-form");
+// console.log(loginForm);
+loginForm.addEventListener("submit", handleLogin);
 
+function handleLogin(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value && password.value) {
+    const loginInfo = { email: email.value, password: password.value };
+    console.log(loginInfo);
+    event.currentTarget.reset();
+  } else {
+    window.alert("Fill in all fields, please!");
+  }
+}
+
+//  Another (opposite) variant to handle login form logic
+//   if (!email.value || !password.value) {
+//     window.alert("Fill in all fields, please!");
+//   } else {
+//     const loginInfo = { email: email.value, password: password.value };
+//     console.log(loginInfo);
+//     event.currentTarget.reset();
+//   }
